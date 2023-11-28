cfbf09e334d3f7aa393922d9030e4ae70ce895e7
cristiansandu22 <cristian.sandu@welthee.com>
2023-11-28 11:36:41 +0200

    Layout
    Layout without styling.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,43 @@ import './App.css'
 const App = () => {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='categories-container'>
+        <div className='category-container'>
+            <div className='category-body-container'>
+                <h1>Hats</h1>
+                <p>Shop Now!</p>
+            </div>
+        </div>
+
+        <div className='category-container'>
+            <div className='category-body-container'>
+                <h1>Jackets</h1>
+                <p>Shop Now!</p>
+            </div>
+        </div>
+
+        <div className='category-container'>
+            <div className='category-body-container'>
+                <h1>Sneakers</h1>
+                <p>Shop Now!</p>
+            </div>
+        </div>
+
+        <div className='category-container'>
+            <div className='category-body-container'>
+                <h1>Women</h1>
+                <p>Shop Now!</p>
+            </div>
+        </div>
+
+        <div className='category-container'>
+            <div className='category-body-container'>
+                <h1>Mens</h1>
+                <p>Shop Now!</p>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
